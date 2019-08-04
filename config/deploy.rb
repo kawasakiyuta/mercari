9e365ff6a6f1851896ab1709dd2585ece15b7f0b
zukupan <y.kawasakicom@gmail.com>
2019-08-04 12:55:46 +0900

    Basic認証の値を環境変数に変更

--- config/deploy.rb
+++ config/deploy.rb
@@ -62,3 +62,10 @@ ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Uncomment the following to require manually verifying the host key before first deploy.
 # set :ssh_options, verify_host_key: :secure
+
+set :default_env, {
+  # AWS_ACCESS_KEY_ID: ENV["AWS_ACCESS_KEY_ID"],
+  # AWS_SECRET_ACCESS_KEY: ENV["AWS_SECRET_ACCESS_KEY"],
+  BASIC_AUTH_USER: ENV["BASIC_AUTH_USER"],
+  BASIC_AUTH_PASSWORD: ENV["BASIC_AUTH_PASSWORD"]
+}
