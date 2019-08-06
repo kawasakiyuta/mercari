3816a777638946b4e892f071ebd96bb8d679e05e
satoshikizuki <sktfhj@icloud.com>
2019-08-06 20:45:07 +0900

    update

---
+++ app/controllers/login_controller.rb
@@ -0,0 +1,25 @@
+class LoginController < ApplicationController
+  class LoginController < ApplicationController
+  
+    def index
+    end
+  
+    def new
+      @user = User.new
+    end
+  
+    def create
+      @user = User.new(params[:user])
+      @user.save
+      redirect_to new_user_path
+    end
+  
+    private
+  
+    def user_params
+      params.require(:user).permit(:name)
+    end
+    
+  end
+  
+end
