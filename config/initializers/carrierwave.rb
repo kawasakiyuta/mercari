adf501b58c3ec2b41e73b7e0f27f0bf570cafd30
zukupan <y.kawasakicom@gmail.com>
2019-08-11 19:05:05 +0900

    resolve_error

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,15 +3,20 @@ require 'carrierwave/storage/file'
 require 'carrierwave/storage/fog'
 
 CarrierWave.configure do |config|
-  config.storage = :fog
-  config.fog_provider = 'fog/aws'
-  config.fog_credentials = {
-    provider: 'AWS',
-    aws_access_key_id: Rails.application.secrets.aws_access_key_id,
-    aws_secret_access_key: Rails.application.secrets.aws_secret_access_key,
-    region: 'ap-northeast-1' #例 'ap-northeast-1'
-  }
-
-  config.fog_directory  = 'upload-test-zkpn'
-  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/upload-test-zkpn'
+  if Rails.env.development?
+    storage :file
+  elsif Rails.env.test?
+    storage :file
+  else
+    config.storage = :fog
+    config.fog_provider = 'fog/aws'
+    config.fog_credentials = {
+      provider: 'AWS',
+      aws_access_key_id: Rails.application.secrets.aws_access_key_id,
+      aws_secret_access_key: Rails.application.secrets.aws_secret_access_key,
+      region: 'ap-northeast-1' #例 'ap-northeast-1'
+    }
+    config.fog_directory  = 'upload-test-zkpn'
+    config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/upload-test-zkpn'
+  end
 end 
