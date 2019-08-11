8a3e9d076d2f217235ea09333952317c19c6a8c9
satoshikizuki <sktfhj@icloud.com>
2019-08-11 19:35:06 +0900

    update

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,9 @@ Rails.application.routes.draw do
   resouces :login, only: :index
   resouces :sign_up, only: [:index, :new]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+ 
+  resources :profile 
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'items#index'
 
 end
