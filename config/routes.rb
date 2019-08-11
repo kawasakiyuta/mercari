506f8c5f776829ac6deb5b40827755050ac00caf
satoshikizuki <sktfhj@icloud.com>
2019-08-11 18:53:49 +0900

    update

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
+  resources :login, only: :index
+  resources :sign_up, only: [:index, :new]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  # root 'login#index'
-  root 'sign_up#new'
 
   # get 'login' => 'login#new'
   # post 'login' => 'login#create'
   
-  # root 'sign_up#index'
+
 end
