27e48544ce74843e36627018fa7db5f8396b59d2
satoshikizuki <sktfhj@icloud.com>
2019-08-11 18:55:07 +0900

    update

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,6 @@ Rails.application.routes.draw do
   resources :login, only: :index
   resources :sign_up, only: [:index, :new]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
-
-  # get 'login' => 'login#new'
-  # post 'login' => 'login#create'
   
 
 end
