b03333246838d7e1da79637cec30e26293eaf03e
zukupan <y.kawasakicom@gmail.com>
2019-08-13 21:18:56 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   resources :profile ,only: [:index]
   resources :logout ,only: [:index]
+  resources :item
+  resources :mypage
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root 'mypage#identification'
+  get 'mypage/indetification', to: 'mypage#identification'
 
 end
