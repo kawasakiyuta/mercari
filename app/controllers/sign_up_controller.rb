62e9be91a8b455653c5997b0f4a0c4d396ee1d01
satoshikizuki <sktfhj@icloud.com>
2019-08-15 19:47:51 +0900

    update

--- app/controllers/sign_up_controller.rb
+++ app/controllers/sign_up_controller.rb
@@ -1,9 +1,10 @@
 class SignUpController < ApplicationController
-    
+  layout 'index'
+
   def index
   end
 
   def new
   end
-  
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
-  resouces :login, only: :index
-  resouces :sign_up, only: [:index, :new]
+
+  resources :login, only: :index
+  resources :sign_up, only: [:index, :new]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
  
-  resources :profile 
+  # resources :profile
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'items#index'
-
+  # root 'items#index'
+  root 'tops#index'
 end
