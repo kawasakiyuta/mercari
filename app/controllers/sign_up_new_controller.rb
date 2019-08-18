de2e869d4e0695c73c239eb9407b187dfbb8e8cc
satoshikizuki <sktfhj@icloud.com>
2019-08-18 13:29:03 +0900

    update

---
+++ app/controllers/sign_up_new_controller.rb
@@ -0,0 +1,19 @@
+class SignUpNewController < ApplicationController
+  layout 'index'
+
+  def index
+  end
+
+  def telephone
+  end
+  
+  def address
+  end
+
+  def pay
+  end
+
+  def create
+  end
+
+end
