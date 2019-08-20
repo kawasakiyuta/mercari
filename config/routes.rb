79e8f66b3adc0939475ba3490503233f573a63f4
satoshikizuki <sktfhj@icloud.com>
2019-08-20 21:47:45 +0900

    update

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,12 @@ Rails.application.routes.draw do
 
   resources :login, only: :index
   resources :sign_up, only: [:index, :new]
-  resources :sign_up_new, only: :index
+  resources :sign_up_new
   
-  get 'sign_up_new/telephone', to: 'sign_up_new#telephone'
-  get 'sign_up_new/address', to: 'sign_up_new#address'
-  get 'sign_up_new/create', to: 'sign_up_new#create'
-  get 'sign_up_new/pay', to: 'sign_up_new#pay'
+  # get 'sign_up_new/telephone', to: 'sign_up_new#telephone'
+  # get 'sign_up_new/address', to: 'sign_up_new#address'
+  # get 'sign_up_new/create', to: 'sign_up_new#create'
+  # get 'sign_up_new/pay', to: 'sign_up_new#pay'
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
