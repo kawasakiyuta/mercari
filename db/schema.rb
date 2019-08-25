5b3ab2532255b7a4625446bce5d976ed6c05f511
maiko05 <maikorto5@icloud.com>
2019-08-26 08:51:24 +0900

    update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -23,13 +23,6 @@ ActiveRecord::Schema.define(version: 2019_08_25_032428) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name"
-    t.string "ancestry"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "user_id", null: false
     t.bigint "product_id", null: false
