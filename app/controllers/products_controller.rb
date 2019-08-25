134d36faadead77dde7e7c035015075373c8227c
taku0000 <t.koyama0123@gmail.com>
2019-08-25 19:25:34 +0900

    一覧、配列表示
    配列を定義しビューへ反映

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -10,9 +10,15 @@ class ProductsController < ApplicationController
   end
   
   def index
-    @name =  "いぬ,俺いぬ、飛んでるいぬ、途中から消える途中から"
-    @price = "33333333"
-    @image = "https://scontent-lga3-1.cdninstagram.com/vp/62f3f45922905dafafe577813b3498d5/5E10DE4F/t51.2885-15/e35/c150.0.601.601a/66607752_161037741733443_119877856891125158_n.jpg?_nc_ht=scontent-lga3-1.cdninstagram.com"
+    
+    @items = [{name:"いぬ,俺いぬ、飛んでるいぬ、途中から消える途中から",price:194782,image:"https://scontent-lga3-1.cdninstagram.com/vp/62f3f45922905dafafe577813b3498d5/5E10DE4F/t51.2885-15/e35/c150.0.601.601a/66607752_161037741733443_119877856891125158_n.jpg?_nc_ht=scontent-lga3-1.cdninstagram.com"},
+    {name:"いぬやけど飛んでます、途中から消える途中から消える",price:333333,image:"https://photo1.ganref.jp/photo/0/8099b8acdc35cde874b69251df88aed6/thumb5.jpg"},
+    {name:"いぬ,俺いぬ、飛んでるいぬ、途中から消える途中から",price:44444,image:"https://twihash.com/wp-content/uploads/2019/07/UBXIkCKPl7.jpg"},
+    {name:"いぬ,俺いぬ、飛んでるいぬ、途中から消える途中から",price:44444,image:"http://pbs.twimg.com/media/CBA_xL_VIAAOvJK.jpg"}
+    ]
+
+
+    
   end
 
   def error  
