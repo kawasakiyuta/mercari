ed0ac0f59653f9847c7a49bd289f101ec350823e
Ricardo Utino <rickutino@gmail.com>
2019-08-25 12:21:17 +0900

    Model 修正

--- db/migrate/20190825031322_create_likes.rb
+++ db/migrate/20190825031322_create_likes.rb
@@ -2,7 +2,7 @@ class CreateLikes < ActiveRecord::Migration[5.2]
   def change
     create_table :likes do |t|
       t.references :user, foreign_key: true, null: false
-      t.references :product_id, foreign_key: true, null: false
+      t.references :product, foreign_key: true, null: false
 
       t.timestamps
     end
