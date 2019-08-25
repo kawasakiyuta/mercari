26c33d1f93bef4e7bae73c1d9ae68f97e186dc20
taku0000 <t.koyama0123@gmail.com>
2019-08-25 18:01:24 +0900

    商品情報、変数表示
    商品情報、変数表示

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,6 +2,7 @@ class ProductsController < ApplicationController
   # before_action :error , only:[:show]
 
   def show
+    
   end
 
   def new
@@ -9,7 +10,9 @@ class ProductsController < ApplicationController
   end
   
   def index
-    
+    @name =  "いぬ,俺いぬ、飛んでるいぬ、途中から消える途中から"
+    @price = "33333333"
+    @image = "https://scontent-lga3-1.cdninstagram.com/vp/62f3f45922905dafafe577813b3498d5/5E10DE4F/t51.2885-15/e35/c150.0.601.601a/66607752_161037741733443_119877856891125158_n.jpg?_nc_ht=scontent-lga3-1.cdninstagram.com"
   end
 
   def error  
