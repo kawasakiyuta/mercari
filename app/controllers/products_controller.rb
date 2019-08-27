be1c6be5aac2d885d0579a4e0152a8e787fb38e5
Ricardo Utino <rickutino@gmail.com>
2019-08-28 00:17:24 +0900

    test

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,15 +1,23 @@
 class ProductsController < ApplicationController
+  # before_action :error , only:[:show]
+
   def show
   end
 
   def new
     @addresses = Address.all
-    render layout: 'index'
   end
-
+  
   def index
+    
   end
 
-  def error
+  def error  
+    # unless 商品があるか
+    #   redirect_to('products/error')
+    # end
+    # サーバーサイド実装後修正
   end
+
+
 end
