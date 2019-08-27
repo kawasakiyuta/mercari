961fe9884bbbc205fc1ca640f413a3fbc4baa5c3
zukupan <y.kawasakicom@gmail.com>
2019-08-27 23:32:01 +0900

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-
-ActiveRecord::Schema.define(version: 2019_08_25_032428) do
-
+ActiveRecord::Schema.define(version: 2019_08_26_112920) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-
 
   create_table "brands", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -27,6 +23,13 @@ ActiveRecord::Schema.define(version: 2019_08_25_032428) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "cards", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.string "customer_id", null: false
+    t.string "card_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
@@ -34,8 +37,6 @@ ActiveRecord::Schema.define(version: 2019_08_25_032428) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-
-
 
   create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "user_id", null: false
@@ -100,7 +101,6 @@ ActiveRecord::Schema.define(version: 2019_08_25_032428) do
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_products_on_user_id"
   end
-
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
