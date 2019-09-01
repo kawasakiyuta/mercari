af20210f0e4c3df854f8eb10c883db846fb925d9
zukupan <y.kawasakicom@gmail.com>
2019-09-01 15:54:38 +0900

    delete email column

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2019_08_26_112920) do
+ActiveRecord::Schema.define(version: 2019_08_28_123007) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -23,7 +22,6 @@ ActiveRecord::Schema.define(version: 2019_08_26_112920) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-
 
   create_table "cards", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "user_id", null: false
@@ -111,13 +109,13 @@ ActiveRecord::Schema.define(version: 2019_08_26_112920) do
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "email"
     t.string "nickname", null: false
     t.date "birthday", null: false
     t.string "first_name", null: false
@@ -131,6 +129,7 @@ ActiveRecord::Schema.define(version: 2019_08_26_112920) do
     t.string "block", null: false
     t.string "building"
     t.integer "wallet", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
