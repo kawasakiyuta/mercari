6a520c61cb8cf6b55743598e8d814a153c5e78fa
Ricardo Utino <rickutino@gmail.com>
2019-09-01 16:41:05 +0900

    ルーティング修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
-  resources :users do
-    collection do
-      get 'logout'
-      get 'identification'
-      get 'mypage'
-    end
-  end 
-  
-
-
+  devise_for :users 
   resources :users do
     collection do
       get 'login'
@@ -21,28 +11,18 @@ Rails.application.routes.draw do
       get 'mypage'
     end
   end
-
-
   root 'products#index'
-
-  resources :products,only: [:index]
-  resources :login, only: :index
+  resources :login, only: [:index, :new, :create]
   resources :sign_up, only: [:index, :new]
-  resources :sign_up_new
-  root 'products#index'
-  get 'products/error' => 'products#error'
-  resources :products
   resources :mypages, only: [:index]
   resources :profile ,only: [:index]
   resources :logout ,only: [:index]
-  get "cards/create", to: "card#create"
-  resources :cards
-  resources :purchase_confirmation
-  resources :products do
+  resources :cards,only: [:index, :show, :create]
+  resources :purchase_confirmation ,only: [:index]
+  resources :products,only: [:index, :show, :new] do
     collection do
       get 'error'
     end
   end
-  get 'mypage/indetification', to: 'mypage#identification'
 
 end
