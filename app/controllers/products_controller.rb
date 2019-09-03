d81a1c8a885ee912c5df6e375cd356654dc43b3e
maiko05 <maikorto5@icloud.com>
2019-09-03 19:30:24 +0900

    update products_controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -20,4 +20,12 @@ class ProductsController < ApplicationController
   end
 
 
+  def destroy
+    product = Product.find(params[:id])
+    if product.user_id == current_user.id
+      product.destroy
+      redirect_to action: :index
+    end
+  end
+
 end
