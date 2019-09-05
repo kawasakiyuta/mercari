fc62dcfd59069eb78ccdd16eb8cb70bcded9b44e
tamura-tatuya <tatuya661199@gmail.com>
2019-09-05 21:53:29 +0900

    アソシエーションの定義

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,8 @@
 class Product < ApplicationRecord
+  belongs_to :user
+  has_many   :comments   ,dependent: :destroy
+  has_many   :likes,dependent: :destroy
+  has_many   :images   ,dependent: :destroy
+  belongs_to :category
+
 end
