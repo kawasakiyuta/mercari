fc61ba39787f3f4e14ae452b1bec53bb903fd45c
maiko05 <maikorto5@icloud.com>
2019-09-15 16:39:57 +0900

    update payjp.js(不要な部分の削除)

--- app/assets/javascripts/payjp.js
+++ app/assets/javascripts/payjp.js
@@ -33,32 +33,3 @@ document.addEventListener(
   },
   false
 );
-
-
-
-// var form = $("#card__form");
-//   Payjp.setPublicKey("pk_test_8c9bf832da690b7c0102c484");
-//   $("#submit_btn").on("click",function(e){
-//     e.preventDefault();
-//     var card = {
-//       number: $("#card_number").val(),
-//       cvc: $("#card_cvc").val(),
-//       exp_month: $("#card_month").val(),
-//       exp_year: $("#card_year").val()
-//     };
-//     Payjp.createToken(card,function(status,response){
-//       form.find("input[type=submit]").prop("disabled", true);
-//       if(status == 200){//←うまくいった場合200になるので
-//         $("#card_number").removeAttr("name");
-//         $("#card_cvc").removeAttr("name");
-//         $("#card_month").removeAttr("name");
-//         $("#card_year").removeAttr("name");
-//         var payjphtml = `<input type="hidden" name="payjpToken" value=${response.id}>`
-//         form.append(payjphtml);
-//         document.inputForm.submit();
-//         alert("登録が完了しました");
-//       }else{
-//         alert("カード情報が正しくありません。");
-//       }
-//     });
-//   });
