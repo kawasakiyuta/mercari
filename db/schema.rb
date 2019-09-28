21d8596c4bec0316a6f51631fc2d1dee357b3afa
tamura-tatuya <tatuya661199@gmail.com>
2019-09-28 19:28:34 +0900

    imagesフォルダをpublic直下に移動

--- db/schema.rb
+++ db/schema.rb
@@ -68,8 +68,8 @@ ActiveRecord::Schema.define(version: 2019_09_22_113149) do
   create_table "images", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "product_id", null: false
     t.string "image", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.index ["product_id"], name: "index_images_on_product_id"
   end
 
@@ -95,19 +95,19 @@ ActiveRecord::Schema.define(version: 2019_09_22_113149) do
   create_table "products", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
     t.string "state", null: false
-    t.string "price", null: false
+    t.string "price", default: "", null: false
     t.boolean "sold"
-    t.bigint "user_id"
+    t.bigint "user_id", null: false
     t.integer "buyer_id"
-    t.string "cost_bearer", null: false
-    t.string "delivery_method"
+    t.string "cost_bearer", default: ""
+    t.string "delivery_method", null: false
     t.string "delivery_souce", null: false
     t.integer "day_to_ship", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.bigint "category_id", null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.string "brand"
-    t.string "size"
+    t.string "size", default: "", null: false
+    t.bigint "category_id", null: false
     t.integer "child_category"
     t.integer "grandchild_category"
     t.index ["category_id"], name: "index_products_on_category_id"
