a1e2555b70014c31f232df05ada4931acc1c788a
Ricardo Utino <rickutino@gmail.com>
2019-09-29 17:50:26 +0900

    別ブランチにカラムエラーを修正する為

--- db/migrate/20190922113149_add_column_products.rb
+++ db/migrate/20190922113149_add_column_products.rb
@@ -1,7 +1,5 @@
 class AddColumnProducts < ActiveRecord::Migration[5.2]
   def change
-    add_column :products, :coment,:text,null: false
-
     add_column :products, :child_category,:integer
 
     add_column :products, :grandchild_category,:integert
