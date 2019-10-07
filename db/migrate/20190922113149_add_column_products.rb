787cf9dbb5c2d2450da4a1586fe25c9c75294c02
zukupan <y.kawasakicom@gmail.com>
2019-10-07 19:58:23 +0900

    Update 20190922113149_add_column_products.rb

--- db/migrate/20190922113149_add_column_products.rb
+++ db/migrate/20190922113149_add_column_products.rb
@@ -2,6 +2,6 @@ class AddColumnProducts < ActiveRecord::Migration[5.2]
   def change
     add_column :products, :child_category,:integer
 
-    add_column :products, :grandchild_category,:integert
+    add_column :products, :grandchild_category,:integer
   end
 end
