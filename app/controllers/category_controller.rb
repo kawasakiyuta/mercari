3d5600dab5ab95c11cee42bcbd0081a832466a40
Ricardo Utino <rickutino@gmail.com>
2019-10-27 21:58:53 +0900

    カテゴリーの表示

--- app/controllers/category_controller.rb
+++ app/controllers/category_controller.rb
@@ -3,5 +3,10 @@ class CategoryController < ApplicationController
     @parents = Category.where(ancestry: nil)
   end
   def show
+    @parents = Category.where(ancestry: nil)
+    @category = Category.find(params[:id])
+    @products = Product.where(category: params[:id])
+    @child_category = Product.where(child_category: params[:id])
+    @grandchild_category = Product.where(grandchild_category: params[:id])
   end
 end
